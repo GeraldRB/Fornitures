65be54a40cec8a1e1a739e903e2d03b498f8dbc9
Gerald <gerald050011@hotmail.com>
2026-03-31 18:06:01 -0600

    Update #6
    Fix Bug of number, change and save the script sendMessage

--- Assets/JS/sendMessage.js
+++ Assets/JS/sendMessage.js
@@ -1,6 +1,4 @@
 function enviarWhatsApp() {
-
-  // Tomar datos del HTML automáticamente
   const nombre = document.querySelector(".product-title").innerText;
   const precio = document.querySelector(".product-price").innerText;
 
@@ -11,6 +9,29 @@ Precio visto en la web: ${precio}.
 ¿Podría brindarme más información?`;
 
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
-
   window.open(url, "_blank");
 }
+
+function changeImage(element) {
+  const mainImage = document.getElementById("mainImage");
+  mainImage.src = element.src;
+
+  document.querySelectorAll(".product-thumb").forEach(t => {
+    t.classList.remove("active");
+  });
+
+  element.parentElement.classList.add("active");
+}
+
+window.changeImage = changeImage;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".product-whatsapp-btn");
+
+  if (btn) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      enviarWhatsApp();
+    });
+  }
+});
